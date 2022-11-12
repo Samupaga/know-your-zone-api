f4c6edbc5faae721e005dba38205ca25afd86c9c
fsaiz97 <felixsaiz97@gmail.com>
2022-11-12 16:31:40 +0000

    Refactor religion database setup religion table now references borough table

--- data/database/setup-db-generic.js
+++ data/database/setup-db-generic.js
@@ -12,15 +12,19 @@ async function addData (dataCategory) {
         console.log("Setup complete.")
 
         const boroughData = await db.query("SELECT id, borough_name FROM borough ORDER BY id")
-        const boroughNames = {}
-        boroughData.rows.forEach(borough => boroughNames[borough.borough_name] = borough.id)
+        const boroughs = {}
+        boroughData.rows.forEach(borough => boroughs[borough.borough_name] = borough.id)
         
         fs.readFile(`data/data-cleaning/converted_data/${dataCategory}_data.csv`, (err, data) => {
             parse(data, {columns:false, trim:true}, async (err, rows) => {
                 rowData = rows.slice(1)
                 for (row of rows.slice(1)) {
+                    const boroughId = boroughs[row[0]]
                     if (dataCategory === "ethnicity") {
-                        await db.query("INSERT INTO ethnicity_data (borough_id, white, asian, black, other, total_population) VALUES ($1, $2, $3, $4, $5, $6)", [boroughNames[row[0]], row[1], row[2], row[3], row[4], row[5]])
+                        await db.query("INSERT INTO ethnicity_data (borough_id, white, asian, black, other, total_population) VALUES ($1, $2, $3, $4, $5, $6)", [boroughId
+                            , row[1], row[2], row[3], row[4], row[5]])
+                    } else if (dataCategory === 'religion') {
+                        await db.query("INSERT INTO religion_data (borough_id, christian, buddhist, hindu, jewish, muslim, sikh, other_religion, no_religion, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [boroughId, row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]])
                     }
                 }
             })
--- data/database/setup-religion.sql
+++ data/database/setup-religion.sql
@@ -2,15 +2,18 @@ DROP TABLE IF EXISTS religion_data;
 
 CREATE TABLE religion_data (
     id INT GENERATED ALWAYS AS IDENTITY, 
-    borough_name VARCHAR(100), 
+    borough_id INT, 
     christian INT, 
     buddhist INT, 
     hindu INT,	
     jewish INT, 
-    muslim	INT,
+    muslim INT,
     sikh INT,
     other_religion INT,
     no_religion INT,	
     total INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_borough
+    FOREIGN KEY (borough_id)
+    REFERENCES borough(id)
 );
