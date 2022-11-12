8ee7d52e83132e3b6c043914e8f502c108d91381
fsaiz97 <felixsaiz97@gmail.com>
2022-11-12 20:02:33 +0000

    Add script to delete all database tables

---
+++ data/database/delete-all-tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS crime_data;
+DROP TABLE IF EXISTS ethnicity_data; 
+DROP TABLE IF EXISTS religion_data;
+DROP TABLE IF EXISTS rental_data;
+DROP TABLE IF EXISTS wellbeing_data;
+DROP TABLE IF EXISTS borough;
