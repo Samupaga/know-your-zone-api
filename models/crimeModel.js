12945e5c43a10b8031584d28d1f56d8033a1c6b4
fsaiz97 <felixsaiz97@gmail.com>
2022-11-14 11:42:33 +0000

    Update summary route summaryModel.js - Get london average for rent and crime - Compare against borough averages - Return as boolean whether borough averages are lower than london - Update Summary constructor with new info rentModel.js - Fix formatting crimeModel.js - Add method to get London crime average

--- models/crimeModel.js
+++ models/crimeModel.js
@@ -8,6 +8,15 @@ class CrimeData {
         this.offence_count = offence_count
 }
 
+    // get average crime count by for london
+    static async getAllLondon() {
+        const population = 8.982e6
+        const popPer1000 = population/1000
+        const response = await db.query(`SELECT (SUM(offence_count::real)/6)/$1 AS london_six_month_crime_rate FROM "public"."crime_data" WHERE period IN (SELECT DISTINCT period FROM crime_data ORDER BY period DESC LIMIT 6)`, [popPer1000])
+
+        return response.rows[0]
+    }
+
     // get average crime count by borough 
     static async getCrimeByBorough(boroughName) {
         const population = 400000
--- models/summaryModel.js
+++ models/summaryModel.js
@@ -2,17 +2,24 @@ const RentalData = require('./rentModel')
 const CrimeData = require('./crimeModel')
 
 class Summary {
-    constructor(borough_name, average_monthly_rent, rent_below_london_average=false, crime_rate_per_1000) {
+    constructor(borough_name, average_monthly_rent, rent_below_london_average, crime_rate_per_1000, crime_below_london_average) {
         this.borough_name = borough_name
         this.average_monthly_rent = average_monthly_rent
         this.rent_below_london_average = rent_below_london_average
         this.crime_rate_per_1000 = crime_rate_per_1000
+        this.crime_below_london_average = crime_below_london_average
     }
 
     static async getOneByName(boroughName) {
         const rentalResponse = await RentalData.getRentByBorough(boroughName)
+        const londonRentResponse = await RentalData.getAllLondon()
+        const rentBelowLondon = rentalResponse.rent_median < londonRentResponse.rent_median
+
         const crimeResponse = await CrimeData.getCrimeByBorough(boroughName)
-        return new Summary(boroughName, rentalResponse.rent_median, false, crimeResponse);
+        const londonCrimeResponse = await CrimeData.getAllLondon()
+        const crimeBelowLondon = crimeResponse.six_month_crime_rate_per_1000 < londonCrimeResponse.london_six_month_crime_rate
+
+        return new Summary(boroughName, rentalResponse.rent_median, rentBelowLondon, crimeResponse.six_month_crime_rate_per_1000, crimeBelowLondon);
     }
 }
 
