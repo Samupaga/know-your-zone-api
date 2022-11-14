8eacc308a312c1111bc02de0929131e1f1a1f2c2
fsaiz97 <felixsaiz97@gmail.com>
2022-11-14 10:21:08 +0000

    Remove age__data from delete-all-tables.sql Manually deleted from database, no need for it in code

--- data/database/delete-all-tables.sql
+++ data/database/delete-all-tables.sql
@@ -4,6 +4,5 @@ DROP TABLE IF EXISTS religion_data;
 DROP TABLE IF EXISTS rental_data;
 DROP TABLE IF EXISTS wellbeing_data;
 DROP TABLE IF EXISTS sex_data;
-DROP TABLE IF EXISTS age_data;
-DROP TABLE IF EXISTS age__data; 
+DROP TABLE IF EXISTS age_data; 
 DROP TABLE IF EXISTS borough;
