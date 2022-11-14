d9d8cfc7093d407a00060341493869938fcfa213
fsaiz97 <felixsaiz97@gmail.com>
2022-11-14 11:57:57 +0000

    Add borough motto to summary

--- models/summaryModel.js
+++ models/summaryModel.js
@@ -1,13 +1,21 @@
 const RentalData = require('./rentModel')
 const CrimeData = require('./crimeModel')
+const db = require('../data/database/db')
 
 class Summary {
-    constructor(borough_name, average_monthly_rent, rent_below_london_average, crime_rate_per_1000, crime_below_london_average) {
+    constructor(borough_name, average_monthly_rent, rent_below_london_average, crime_rate_per_1000, crime_below_london_average, motto) {
         this.borough_name = borough_name
         this.average_monthly_rent = average_monthly_rent
         this.rent_below_london_average = rent_below_london_average
         this.crime_rate_per_1000 = crime_rate_per_1000
         this.crime_below_london_average = crime_below_london_average
+        this.motto = motto
+    }
+
+    static async getBoroughMotto(boroughName) {
+        const response = await db.query("SELECT motto FROM borough WHERE borough_name = $1", [boroughName])
+
+        return response.rows[0].motto
     }
 
     static async getOneByName(boroughName) {
@@ -19,7 +27,9 @@ class Summary {
         const londonCrimeResponse = await CrimeData.getAllLondon()
         const crimeBelowLondon = crimeResponse.six_month_crime_rate_per_1000 < londonCrimeResponse.london_six_month_crime_rate
 
-        return new Summary(boroughName, rentalResponse.rent_median, rentBelowLondon, crimeResponse.six_month_crime_rate_per_1000, crimeBelowLondon);
+        const motto = await Summary.getBoroughMotto(boroughName)
+
+        return new Summary(boroughName, rentalResponse.rent_median, rentBelowLondon, crimeResponse.six_month_crime_rate_per_1000, crimeBelowLondon, motto);
     }
 }
 
