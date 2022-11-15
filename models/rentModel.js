225805e9c4ab65ec9e9875a0f686c9a23e3187e2
fsaiz97 <felixsaiz97@gmail.com>
2022-11-15 17:38:31 +0000

    Update rentModel.js Change getRentByBorough response to return data for all periods

--- models/rentModel.js
+++ models/rentModel.js
@@ -21,9 +21,9 @@ class RentalData {
     }
 
     static async getRentByBorough(boroughName) {
-        const response = await db.query("SELECT borough_name, period_start_date, period_end_date, property_type, rent_median, rent_mean FROM rental_data JOIN borough ON rental_data.borough_id = borough.id WHERE borough_name = $1 AND property_type = 'All categories' AND period_end_date = (SELECT DISTINCT period_end_date FROM rental_data ORDER BY period_end_date DESC LIMIT 1)", [boroughName])
+        const response = await db.query("SELECT borough_name, period_start_date, period_end_date, property_type, rent_median, rent_mean FROM rental_data JOIN borough ON rental_data.borough_id = borough.id WHERE borough_name = $1 AND property_type = 'All categories' ORDER BY period_start_date DESC", [boroughName])
 
-        return new RentalData(response.rows[0])
+        return response.rows.map(elem => new RentalData(elem))
     }
 
     static async getRentPerCategoryByBorough(borough_name) {
