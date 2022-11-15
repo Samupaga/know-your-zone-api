e65907cb622c28d7ff1f929eefdd4d8c0235d1f4
Samupaga <sam.paga.uele@gmail.com>
2022-11-15 13:00:14 +0000

    added second_lang to sumamry object return

--- models/summaryModel.js
+++ models/summaryModel.js
@@ -3,12 +3,13 @@ const CrimeData = require('./crimeModel')
 const db = require('../data/database/db')
 
 class Summary {
-    constructor(borough_name, average_monthly_rent, rent_below_london_average, crime_rate_per_1000, crime_below_london_average, motto) {
+    constructor(borough_name, average_monthly_rent, rent_below_london_average, crime_rate_per_1000, crime_below_london_average, second_lang, motto) {
         this.borough_name = borough_name
         this.average_monthly_rent = average_monthly_rent
         this.rent_below_london_average = rent_below_london_average
         this.crime_rate_per_1000 = crime_rate_per_1000
         this.crime_below_london_average = crime_below_london_average
+        this.second_lang = second_lang
         this.motto = motto
     }
 
@@ -16,6 +17,12 @@ class Summary {
         const response = await db.query("SELECT motto FROM borough WHERE borough_name = $1", [boroughName])
 
         return response.rows[0].motto
+    }
+
+    static async getBoroughLang(boroughName) {
+        const response = await db.query("SELECT second_lang FROM borough WHERE borough_name = $1", [boroughName])
+
+        return response.rows[0].second_lang
     }
 
     static async getOneByName(boroughName) {
@@ -27,9 +34,11 @@ class Summary {
         const londonCrimeResponse = await CrimeData.getAllLondon()
         const crimeBelowLondon = crimeResponse.six_month_crime_rate_per_1000 < londonCrimeResponse.london_six_month_crime_rate
 
+        const second_lang = await Summary.getBoroughLang(boroughName)
+
         const motto = await Summary.getBoroughMotto(boroughName)
 
-        return new Summary(boroughName, rentalResponse.rent_median, rentBelowLondon, crimeResponse.six_month_crime_rate_per_1000, crimeBelowLondon, motto);
+        return new Summary(boroughName, rentalResponse.rent_median, rentBelowLondon, crimeResponse.six_month_crime_rate_per_1000, crimeBelowLondon, second_lang, motto);
     }
 }
 
