25b5eb0be10a0e45ce57ea133c46f4c2a1f9fa9c
fsaiz97 <felixsaiz97@gmail.com>
2022-11-16 18:12:56 +0000

    Add household route

--- routers/demoRouter.js
+++ routers/demoRouter.js
@@ -9,6 +9,7 @@ demoRouter.get('/:borough/religion', demoController.showReligion);
 demoRouter.get('/:borough/wellbeing', demoController.showWellbeing);
 demoRouter.get('/:borough/age', demoController.showAge);
 demoRouter.get('/:borough/sex', demoController.showSex);
+demoRouter.get('/:borough/household', demoController.showHousehold);
 // demoRouter.get('/:borough/lifesatisfaction', demoController.showLifeSat); 
 // demoRouter.get('/:borough/worthwhile', demoController.showWorth);
 // demoRouter.get('/:borough/happiness', demoController.showHappiness);
