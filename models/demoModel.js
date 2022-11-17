c13a4e1fe4010fc14818eeada0143c81f36caf44
fsaiz97 <felixsaiz97@gmail.com>
2022-11-17 18:02:57 +0000

    DemoData: Add semicolons everywhere

--- models/demoModel.js
+++ models/demoModel.js
@@ -2,86 +2,86 @@ const db = require('../data/database/db');
 
 class DemoData {
     constructor (borough_name, type, category_names, ranked, data) {
-        this.borough_name = borough_name
-        this.type = type
-        this.category_names = category_names
-        this.ranked = ranked
-        this.data = data
+        this.borough_name = borough_name;
+        this.type = type;
+        this.category_names = category_names;
+        this.ranked = ranked;
+        this.data = data;
     }
 
     static #queryDataHandler(queryData, isRanked) {
-        const categories = Object.keys(queryData).filter(elem => elem !== 'borough_name')
-        const data = []
+        const categories = Object.keys(queryData).filter(elem => elem !== 'borough_name');
+        const data = [];
 
         for (const category of categories) {
-            data.push({category: category, value: queryData[category]})
+            data.push({category: category, value: queryData[category]});
         }
 
         if (isRanked) {
-            data.sort((a, b) => b.value - a.value)
+            data.sort((a, b) => b.value - a.value);
         }
 
-        return [queryData.borough_name, categories, data]
+        return [queryData.borough_name, categories, data];
     }
 
     static async getEthnicityByBorough(borough_name) {
-        const ranked = true
+        const ranked = true;
         
-        const response = await db.query(`SELECT borough_name, white, black, asian, other FROM ethnicity_data JOIN borough ON ethnicity_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, white, black, asian, other FROM ethnicity_data JOIN borough ON ethnicity_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'ethnicity', categories, ranked, data)
+        return new DemoData(boroughName, 'ethnicity', categories, ranked, data);
     }
 
     static async getReligionByBorough(borough_name) {
-        const ranked = true
+        const ranked = true;
 
-        const response = await db.query(`SELECT borough_name, christian, buddhist, hindu, jewish, muslim, sikh, other_religion, no_religion FROM religion_data JOIN borough ON religion_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, christian, buddhist, hindu, jewish, muslim, sikh, other_religion, no_religion FROM religion_data JOIN borough ON religion_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'religion', categories, ranked, data)
+        return new DemoData(boroughName, 'religion', categories, ranked, data);
     }
 
     static async getWellbeingByBorough(borough_name) {
-        const ranked = false
+        const ranked = false;
 
-        const response = await db.query(`SELECT borough_name, life_satisfaction, worthwhile, happiness, anxiety, wellbeing FROM wellbeing_data JOIN borough ON wellbeing_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, life_satisfaction, worthwhile, happiness, anxiety, wellbeing FROM wellbeing_data JOIN borough ON wellbeing_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'wellbeing', categories, ranked, data)
+        return new DemoData(boroughName, 'wellbeing', categories, ranked, data);
     }
 
     static async getAgeByBorough(borough_name) {
-        const ranked = true
+        const ranked = true;
 
-        const response = await db.query(`SELECT borough_name, a0_9, a10_17, a18_26, a27_35, a36_44, a45_53, a54_62, a63_71, a72_80, a81_ FROM age_data JOIN borough ON age_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, a0_9, a10_17, a18_26, a27_35, a36_44, a45_53, a54_62, a63_71, a72_80, a81_ FROM age_data JOIN borough ON age_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'age', categories, ranked, data)
+        return new DemoData(boroughName, 'age', categories, ranked, data);
     }
 
     static async getSexByBorough(borough_name) {
-        const ranked = false
+        const ranked = false;
 
-        const response = await db.query(`SELECT borough_name, m_100f FROM sex_data JOIN borough ON sex_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, m_100f FROM sex_data JOIN borough ON sex_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'sex', categories, ranked, data)
+        return new DemoData(boroughName, 'sex', categories, ranked, data);
     }
 
     static async getHouseholdByBorough(borough_name) {
-        const ranked = true
+        const ranked = true;
 
-        const response = await db.query(`SELECT borough_name, one_person, married_couple, civil_partnership, cohabiting_couple, lone_parent, multi_person FROM household_data JOIN borough ON household_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name])
+        const response = await db.query(`SELECT borough_name, one_person, married_couple, civil_partnership, cohabiting_couple, lone_parent, multi_person FROM household_data JOIN borough ON household_data.borough_id = borough.id WHERE borough_name = $1`, [borough_name]);
 
-        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked)
+        const [boroughName, categories, data] = DemoData.#queryDataHandler(response.rows[0], ranked);
 
-        return new DemoData(boroughName, 'household', categories, ranked, data)
+        return new DemoData(boroughName, 'household', categories, ranked, data);
     }
 }
 
